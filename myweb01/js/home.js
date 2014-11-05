2e36247683c743b2a4fd9cf502b389429f7267cc
felix <knfelix@163.com>
2014-11-05 09:53:23 +0800

    xdr web 1.0

---
+++ myweb01/js/home.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('.logo').mouseover(function() {
+		$('.wap').addClass('opactiy_ease');
+		});
+	$('.logo').mouseout(function() {
+		$('.wap').removeClass('opactiy_ease');
+		});
+});
---
+++ myweb01/out/js/home-min.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".logo").mouseover(function(){$(".wap").addClass("opactiy_ease")}),$(".logo").mouseout(function(){$(".wap").removeClass("opactiy_ease")})});
