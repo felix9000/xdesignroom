73102f427065177a4bef58c8757306938654d389
felix <knfelix@163.com>
2015-03-16 20:02:13 +0800

    更新work背景

---
+++ myweb01/js/work.js
@@ -0,0 +1,7 @@
+jQuery('.zhuoyujia').hover(function() 
+	{ 
+		jQuery('body').css("background", "url(../img/work/zyj1.jpg) center top #1C1C1C fixed"),
+		jQuery('body').css("background-size", "cover");
+	}).mouseleave(function(){
+		jQuery('body').css("background", "#1C1C1C"); 
+	});
