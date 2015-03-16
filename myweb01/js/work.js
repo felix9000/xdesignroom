12b6cf50e7621034a108b16d103cd06ba1cfae05
felix <knfelix@163.com>
2015-03-16 22:48:36 +0800

    work更新背景

--- myweb01/js/work.js
+++ myweb01/js/work.js
@@ -1,7 +1,53 @@
-jQuery('.zhuoyujia').hover(function() 
+$(document).ready(function(){
+	  $('.zhuoyujia').mouseover(function() 
 	{ 
-		jQuery('body').css("background", "url(../img/work/zyj1.jpg) center top #1C1C1C fixed"),
-		jQuery('body').css("background-size", "cover");
+		$('body').css("background", "url(../img/work/zyj1.jpg) center top #1C1C1C fixed"),
+		$('body').css("background-size", "cover");
+		// $('body').css("background-attachment", "fixed");
 	}).mouseleave(function(){
 		jQuery('body').css("background", "#1C1C1C"); 
 	});
+$('.xingwu').mouseover(function() 
+	{ 
+		$('body').css("background", "url(../img/work/wx1.jpg) center top #1C1C1C fixed"),
+		$('body').css("background-size", "cover");
+	}).mouseleave(function(){
+		jQuery('body').css("background", "#1C1C1C"); 
+	});
+ });
+
+// $(window).resize(function() { 
+//   var wd = $(document.body).width();
+
+//   if(wd <480){
+// 	$('.zhuoyujia').mouseover(function() 
+// 	{ 
+// 		jQuery('body').css("background", "#1C1C1C"); 
+// 	});
+// $('.xingwu').mouseover(function() 
+// 	{ 
+// 		jQuery('body').css("background", "#1C1C1C"); 
+// 	});
+
+// }
+// else{
+// 	$('.zhuoyujia').mouseover(function() 
+// 	{ 
+// 		$('body').css("background", "url(../img/work/zyj1.jpg) center top #1C1C1C fixed"),
+// 		$('body').css("background-size", "cover");
+// 		// $('body').css("background-attachment", "fixed");
+// 	}).mouseleave(function(){
+// 		jQuery('body').css("background", "#1C1C1C"); 
+// 	});
+// $('.xingwu').mouseover(function() 
+// 	{ 
+// 		$('body').css("background", "url(../img/work/wx1.jpg) center top #1C1C1C fixed"),
+// 		$('body').css("background-size", "cover");
+// 	}).mouseleave(function(){
+// 		jQuery('body').css("background", "#1C1C1C"); 
+// 	});
+// }
+//});
+
+
+
