1de5a7bcd0baa7717a2e8af4f9cef327d5fe4cd3
felix <knfelix@163.com>
2015-07-12 16:32:27 +0800

    设计+了解页面
    新增设计了解页面

--- myweb01/js/work.js
+++ myweb01/js/work.js
@@ -1,20 +1,173 @@
 $(document).ready(function(){
-	  $('.zhuoyujia').mouseover(function() 
-	{ 
-		$('.zhuoyujia').css("background", "url(../img/work/zyj1.jpg) center top #1C1C1C fixed"),
-		$('.zhuoyujia').css("background-size", "cover");
-		// $('body').css("background-attachment", "fixed");
-	}).mouseleave(function(){
-		jQuery('.zhuoyujia').css("background", "#1C1C1C"); 
-	});
-$('.xingwu').mouseover(function() 
-	{ 
-		$('.xingwu').css("background", "url(../img/work/wx1.jpg) center top #1C1C1C fixed"),
-		$('.xingwu').css("background-size", "cover");
-	}).mouseleave(function(){
-		jQuery('.xingwu').css("background", "#1C1C1C"); 
-	});
- });
+
+var skTextNub=0
+var htmlTextNub=0
+var cssTextNub=0
+var jsTextNub=0
+var psTextNub=0
+var aiTextNub=0
+var aeTextNub=0
+var dataNub ={
+	numberText:function(){
+		var skText=$(".data_nub p").eq(0).text(skTextNub+"%").innerHTML=skTextNub;
+		var htmlText=$(".data_nub p").eq(1).text(htmlTextNub+"%").innerHTML=htmlTextNub;
+		var cssText=$(".data_nub p").eq(2).text(cssTextNub+"%").innerHTML=cssTextNub;
+		var jsText=$(".data_nub p").eq(3).text(jsTextNub+"%").innerHTML=jsTextNub;
+		var psText=$(".data_nub p").eq(4).text(psTextNub+"%").innerHTML=psTextNub;
+		var aiText=$(".data_nub p").eq(5).text(aiTextNub+"%").innerHTML=aiTextNub;
+		var aeText=$(".data_nub p").eq(6).text(aeTextNub+"%").innerHTML=aeTextNub;
+		$(".bar_sketch").css({"width":skText+"%"});
+		$(".bar_html").css({"width":htmlText+"%"});
+		$(".bar_css").css({"width":cssText+"%"});
+		$(".bar_js").css({"width":jsText+"%"});
+		$(".bar_ps").css({"width":psText+"%"});
+		$(".bar_ai").css({"width":aiText+"%"});
+		$(".bar_ae").css({"width":aeText+"%"});
+
+	}
+
+};
+// dataNub.numberText();
+
+
+
+$(".work_pro .work_object").eq(0).hover(function() { 
+	 // dataNub.numberText();
+var dataNub={
+	numberText:function(){
+		var skText=$(".data_nub p").eq(0).text(skTextNub+"%").innerHTML=skTextNub;
+		var htmlText=$(".data_nub p").eq(1).text(htmlTextNub+"%").innerHTML=htmlTextNub;
+		var cssText=$(".data_nub p").eq(2).text(cssTextNub+"%").innerHTML=cssTextNub;
+		var jsText=$(".data_nub p").eq(3).text(jsTextNub+"%").innerHTML=jsTextNub;
+		var psText=$(".data_nub p").eq(4).text(psTextNub+"%").innerHTML=psTextNub;
+		var aiText=$(".data_nub p").eq(5).text(aiTextNub+"%").innerHTML=aiTextNub;
+		var aeText=$(".data_nub p").eq(6).text(aeTextNub+"%").innerHTML=aeTextNub;
+		$(".bar_sketch").css({"width":skText+"%"});
+		$(".bar_html").css({"width":htmlText+"%"});
+		$(".bar_css").css({"width":cssText+"%"});
+		$(".bar_js").css({"width":jsText+"%"});
+		$(".bar_ps").css({"width":psText+"%"});
+		$(".bar_ai").css({"width":aiText+"%"});
+		$(".bar_ae").css({"width":aeText+"%"});
+	}
+
+};
+	var skTextNub=10
+	var htmlTextNub=0
+	var cssTextNub=0
+	var jsTextNub=0
+	var psTextNub=30
+	var aiTextNub=60
+	var aeTextNub=0
+	 dataNub.numberText();
+	// console.log("123990");
+
+},
+function() { 
+	dataNub.numberText();
+	// console.log("378");
+
+});
+
+
+
+
+$(".work_pro .work_object").eq(1).hover(function() { 
+var skTextNub=0
+var htmlTextNub=0
+var cssTextNub=0
+var jsTextNub=0
+var psTextNub=35
+var aiTextNub=65
+var aeTextNub=0
+var dataNub={
+	numberText:function(){
+		var skText=$(".data_nub p").eq(0).text(skTextNub+"%").innerHTML=skTextNub;
+		var htmlText=$(".data_nub p").eq(1).text(htmlTextNub+"%").innerHTML=htmlTextNub;
+		var cssText=$(".data_nub p").eq(2).text(cssTextNub+"%").innerHTML=cssTextNub;
+		var jsText=$(".data_nub p").eq(3).text(jsTextNub+"%").innerHTML=jsTextNub;
+		var psText=$(".data_nub p").eq(4).text(psTextNub+"%").innerHTML=psTextNub;
+		var aiText=$(".data_nub p").eq(5).text(aiTextNub+"%").innerHTML=aiTextNub;
+		var aeText=$(".data_nub p").eq(6).text(aeTextNub+"%").innerHTML=aeTextNub;
+		$(".bar_sketch").css({"width":skText+"%"});
+		$(".bar_html").css({"width":htmlText+"%"});
+		$(".bar_css").css({"width":cssText+"%"});
+		$(".bar_js").css({"width":jsText+"%"});
+		$(".bar_ps").css({"width":psText+"%"});
+		$(".bar_ai").css({"width":aiText+"%"});
+		$(".bar_ae").css({"width":aeText+"%"});
+	}
+
+};
+dataNub.numberText();
+
+},
+function() { 
+	 dataNub.numberText();
+
+});
+
+
+$(".work_pro .work_object").eq(2).hover(function() { 
+var skTextNub=20
+var htmlTextNub=25
+var cssTextNub=30
+var jsTextNub=15
+var psTextNub=10
+var aiTextNub=0
+var aeTextNub=0
+var dataNub={
+	numberText:function(){
+		var skText=$(".data_nub p").eq(0).text(skTextNub+"%").innerHTML=skTextNub;
+		var htmlText=$(".data_nub p").eq(1).text(htmlTextNub+"%").innerHTML=htmlTextNub;
+		var cssText=$(".data_nub p").eq(2).text(cssTextNub+"%").innerHTML=cssTextNub;
+		var jsText=$(".data_nub p").eq(3).text(jsTextNub+"%").innerHTML=jsTextNub;
+		var psText=$(".data_nub p").eq(4).text(psTextNub+"%").innerHTML=psTextNub;
+		var aiText=$(".data_nub p").eq(5).text(aiTextNub+"%").innerHTML=aiTextNub;
+		var aeText=$(".data_nub p").eq(6).text(aeTextNub+"%").innerHTML=aeTextNub;
+		$(".bar_sketch").css({"width":skText+"%"});
+		$(".bar_html").css({"width":htmlText+"%"});
+		$(".bar_css").css({"width":cssText+"%"});
+		$(".bar_js").css({"width":jsText+"%"});
+		$(".bar_ps").css({"width":psText+"%"});
+		$(".bar_ai").css({"width":aiText+"%"});
+		$(".bar_ae").css({"width":aeText+"%"});
+	}
+
+};
+dataNub.numberText();
+
+},
+function() { 
+	 dataNub.numberText();
+
+});
+
+
+});
+
+
+
+
+
+// 鼠标悬浮全屏背景显示图片效果
+// $(document).ready(function(){
+// 	  $('.zhuoyujia').mouseover(function() 
+// 	{ 
+// 		$('.zhuoyujia').css("background", "url(../img/work/zyj1.jpg) center top #1C1C1C fixed"),
+// 		$('.zhuoyujia').css("background-size", "cover");
+// 		// $('body').css("background-attachment", "fixed");
+// 	}).mouseleave(function(){
+// 		jQuery('.zhuoyujia').css("background", "#1C1C1C"); 
+// 	});
+// $('.xingwu').mouseover(function() 
+// 	{ 
+// 		$('.xingwu').css("background", "url(../img/work/wx1.jpg) center top #1C1C1C fixed"),
+// 		$('.xingwu').css("background-size", "cover");
+// 	}).mouseleave(function(){
+// 		jQuery('.xingwu').css("background", "#1C1C1C"); 
+// 	});
+//  });
 
 // $(window).resize(function() { 
 //   var wd = $(document.body).width();
